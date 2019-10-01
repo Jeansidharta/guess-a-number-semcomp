98346ad1e3410198d5fb1c70318f5ac941caea98
jean sidharta <jeansidharta@gmail.com>
2019-10-01 12:46:15 -0700

    removed redirect and switch from app

--- src/index.js
+++ src/index.js
@@ -3,19 +3,16 @@ import ReactDOM from "react-dom";
 import Home from "./pages/home/home";
 import Game from "./pages/game/game";
 import End from "./pages/end/end";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import "./index.css";
 
 function App(){
 	return (
 		<BrowserRouter>
-			<Switch>
-				<Route path="/home" component={Home}/>
-				<Route path="/game/:number" component={Game}/>
-				<Route path="/end/:number" component={End}/>
-				<Redirect to="/home"/>
-			</Switch>
+			<Route path="/home" component={Home}/>
+			<Route path="/game/:number" component={Game}/>
+			<Route path="/end/:number" component={End}/>
 		</BrowserRouter>
 	);
 }
